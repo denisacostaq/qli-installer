11fc3d147cacec510a1a435f87063245c6eed914
Nelson Chen <crazysim@gmail.com>
2019-05-25 22:31:36 -0700

    oops

--- qli-installer.py
+++ qli-installer.py
@@ -38,7 +38,7 @@ if len(sys.argv) < 4 or len(sys.argv) > 5:
     print("targets:      desktop, android, ios")
     exit(1)
 
-base_url = Path(getenv('QLI_BASE_URL', "https://download.qt.io/online/qtsdkrepository/"))
+base_url = getenv('QLI_BASE_URL', "https://download.qt.io/online/qtsdkrepository/")
 
 # Qt version
 qt_version = sys.argv[1]
